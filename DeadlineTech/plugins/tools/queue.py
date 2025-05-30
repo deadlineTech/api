bcdd40a81e86e1779a627d207ff9c7f584cd525f
naynay11122211 <choustun1290@gmail.com>
2025-05-30 10:33:41 +0630

    Update queue.py

--- DeadlineTech/plugins/tools/queue.py
+++ DeadlineTech/plugins/tools/queue.py
@@ -165,11 +165,11 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
     for x in got:
         j += 1
         if j == 1:
-            msg += f'Streaming :\n\n✨ Title : {x["title"]}\nDuration : {x["dur"]}\nBy : {x["by"]}\n\n'
+            msg += f'စတင်နေသည် :\n\n✨ ခေါင်းစဉ် : {x["title"]}\nကြာချိန် : {x["dur"]}\nBy : {x["by"]}\n\n'
         elif j == 2:
-            msg += f'Queued :\n\n✨ Title : {x["title"]}\nDuration : {x["dur"]}\nBy : {x["by"]}\n\n'
+            msg += f'ဆက်ဖွင့်မည်။ :\n\n✨ ခေါင်းစဉ် : {x["title"]}\nကြာချိန် : {x["dur"]}\nBy : {x["by"]}\n\n'
         else:
-            msg += f'✨ Title : {x["title"]}\nDuration : {x["dur"]}\nBy : {x["by"]}\n\n'
+            msg += f'✨ ခေါင်းစဉ် : {x["title"]}\nကြာချိန် : {x["dur"]}\nBy : {x["by"]}\n\n'
     if "Queued" in msg:
         if len(msg) < 700:
             await asyncio.sleep(1)
